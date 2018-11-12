29588456f2c62c8ad7743ae0274354c279b9c693
Jason Basuil <basuilj@gmail.com>
2018-11-12 20:54:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 def speak_to_grandma(string)
   if string == string.upcase
-    return "I LOVE YOU TOO PUMPKIN!"
+    return "NO, NOT SINCE 1938!"
   elsif string == string.downcase
     return "HUH?! SPEAK UP, SONNY!"
+  else string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
 end
 
